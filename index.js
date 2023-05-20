10b411c45bdcfe9d2010e62b67fdbfc0ceb9b900
Sadik <anarul6777@gmail.com>
2023-05-20 20:36:20 +0600

    delete api is ready

--- index.js
+++ index.js
@@ -60,6 +60,13 @@ async function run() {
       res.send(result);
     })
 
+    app.delete('/cars/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await carCollection.deleteOne(query);
+      res.send(result);
+    })
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
