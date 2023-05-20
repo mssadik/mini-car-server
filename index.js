6424a7945f3399e835cc77527b05911f98462584
Sadik <anarul6777@gmail.com>
2023-05-20 07:32:10 +0600

    dynamic data loaded

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 
@@ -28,6 +28,21 @@ async function run() {
     await client.connect();
 
     const carCollection = client.db('miniCarDB').collection('miniCar');
+
+    app.get('/cars', async(req, res) =>{
+      const result = await carCollection.find().toArray();
+      res.send(result);
+    })
+
+    app.get('/cars/:id', async(req, res) =>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const options = {
+        projection: {decription:1, photo:1, price:1, quantity:1, rating:1, sellerEmail:1, sellerName:1, subCategory:1, toyName:1}
+      };
+      const result = await carCollection.findOne(query, options);
+      res.send(result)
+    })
 
     app.post('/cars', async(req, res) =>{
       const car = req.body;
